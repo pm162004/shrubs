34a00095040c202d1983a502c284a7b9a686a760
pm162004 <pm121614@gmail.com>
2025-05-27 18:50:09 +0530

    Initial commit 2

--- shrubs_web/test_positive_flow.py
+++ shrubs_web/test_positive_flow.py
@@ -1,5 +1,5 @@
 import random
-import time,os,datetime
+import time, os, datetime
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
@@ -27,15 +27,17 @@ time.sleep(3)
 wait = WebDriverWait(driver, 25)
 
 
-
 def display_myfiles_after_login():
     return wait.until(EC.presence_of_element_located((By.XPATH, "//b[normalize-space()='My Files']")))
+
 
 def email_input_field():
     return wait.until(EC.presence_of_element_located((By.NAME, "email")))
 
+
 def password_input_field():
     return wait.until(EC.presence_of_element_located((By.NAME, "password")))
+
 
 def login_button():
     return wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@name='btn-signin']")))
@@ -45,15 +47,20 @@ def refresh_page():
     logger.info("Refreshing page")
     return driver.refresh()
 
+
 def password_mask_button():
     return wait.until(EC.element_to_be_clickable((By.XPATH, "//*[name()='path' and contains(@d,'M12 7c2.76')]")))
 
+
 def check_logout_btn():
-    return wait.until(EC.element_to_be_clickable((By.XPATH,"//span[text()='Logout' and contains(@class, 'user-menu')]")))
+    return wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//span[text()='Logout' and contains(@class, 'user-menu')]")))
+
 
 def quit_browser():
     time.sleep(1)
     driver.quit()
+
 
 def save_screenshot(filename, use_timestamp=True, folder="screenshorts"):
     os.makedirs(folder, exist_ok=True)
@@ -76,29 +83,39 @@ def get_my_shrubs():
 
 def get_new_shrub():
     time.sleep(5)
-    return wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='md-button-content' and text()='New Shrub']")))
+    return wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//div[@class='md-button-content' and text()='New Shrub']")))
+
 
 def shrub_title_input_field():
     return wait.until(EC.presence_of_element_located((By.XPATH, "//input[@name='shrub-name']")))
 
+
 def shrub_sub_header_input_field():
     return wait.until(EC.presence_of_element_located((By.XPATH, "//input[@name='shrub-sub-header']")))
 
+
 def shrub_description_textbox():
-    return wait.until(EC.presence_of_element_located((By.XPATH, "(//div[contains(@class, 'ck-editor__editable') and @contenteditable='true'])[1]")))
+    return wait.until(EC.presence_of_element_located(
+        (By.XPATH, "(//div[contains(@class, 'ck-editor__editable') and @contenteditable='true'])[1]")))
+
 
 def select_view_only_permissions():
     return wait.until(EC.element_to_be_clickable((By.XPATH, "//label[normalize-space()='View Only']")))
 
+
 def select_allow_resharing_permissions():
     return wait.until(EC.element_to_be_clickable((By.XPATH, "//label[normalize-space()='Allow Resharing']")))
 
+
 def select_download_save_permissions():
-    return wait.until(EC.element_to_be_clickable((By.XPATH, "//label[normalize-space()='Download and save to Shrubdrive']")))
+    return wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//label[normalize-space()='Download and save to Shrubdrive']")))
 
 
 def shrub_project_icon_btn():
     return wait.until(EC.element_to_be_clickable((By.XPATH, "//label[normalize-space()='Shrub Project Icon']")))
+
 
 def select_thumbnail_image_btn():
     overlay_spinner()
@@ -108,34 +125,43 @@ def select_thumbnail_image_btn():
 def select_thumbnail_icon():
     return wait.until(EC.element_to_be_clickable((By.XPATH, "//h6[normalize-space()='alert-octagon']")))
 
+
 def thumbnail_icon_cancel_btn():
     return wait.until(EC.element_to_be_clickable((By.XPATH, "//div[contains(text(),'Cancel')]")))
+
 
 def save_new_shrub_btn():
     logger.info("Waiting for save shrub button to be clickable")
     overlay_spinner()
     return WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "btn-save")))
 
+
 def next_image_btn():
     overlay_spinner()
     return wait.until(EC.element_to_be_clickable((By.XPATH, "//button[.//div[normalize-space(text())='Next']]")))
 
+
 def save_crop_image_btn():
     overlay_spinner()
-    return wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@type='button' and .//div[@class='md-button-content' and normalize-space()='Save']]")))
+    return wait.until(EC.element_to_be_clickable(
+        (By.XPATH, "//button[@type='button' and .//div[@class='md-button-content' and normalize-space()='Save']]")))
+
 
 def upload_image_my_files_btn():
     return wait.until(EC.element_to_be_clickable(
-        (By.XPATH, "//div[contains(@class, 'sidebar-card') and .//p[normalize-space(text())='My Files']]//a[contains(@class, 'menu-list-item')]")))
+        (By.XPATH,
+         "//div[contains(@class, 'sidebar-card') and .//p[normalize-space(text())='My Files']]//a[contains(@class, 'menu-list-item')]")))
+
 
 def upload_image_my_shrubs_btn():
     return wait.until(EC.element_to_be_clickable(
-        (By.XPATH, "//div[contains(@class, 'sidebar-card') and .//p[normalize-space(text())='My Shrubs']]//a[contains(@class, 'menu-list-item')]")))
+        (By.XPATH,
+         "//div[contains(@class, 'sidebar-card') and .//p[normalize-space(text())='My Shrubs']]//a[contains(@class, 'menu-list-item')]")))
+
 
 def select_thumbnail_folder():
     return wait.until(EC.element_to_be_clickable(
         (By.XPATH, "//div[span[text()[normalize-space()='thumbnail']]]")))
-
 
 
 def select_random_image(driver):
@@ -150,6 +176,7 @@ def select_random_image(driver):
     except Exception as e:
         raise Exception(f"Failed to select random image: {str(e)}")
 
+
 def zoomin_image_btn():
     overlay_spinner()
     zoom_in_button = wait.until(
@@ -158,6 +185,7 @@ def zoomin_image_btn():
 
     return zoom_in_button.click()
 
+
 def zoom_out_image_btn():
     overlay_spinner()
     zoom_out_button = wait.until(
@@ -165,12 +193,12 @@ def zoom_out_image_btn():
     )
     return zoom_out_button.click()
 
+
 def overlay_spinner():
     return WebDriverWait(driver, 10).until(EC.invisibility_of_element_located((By.ID, "overlay-spinner")))
 
 
 def select_random_icon():
-
     icons = wait.until(EC.visibility_of_all_elements_located(
         (By.CSS_SELECTOR, "div.icon-hover")
     ))
@@ -190,7 +218,6 @@ def upload_random_image(relative_folder):
 
     file_path = os.path.join(folder_path, random.choice(images))
 
-
     file_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='file']")))
     file_input.send_keys(file_path)
 
@@ -203,6 +230,7 @@ def upload_random_image(relative_folder):
     except:
         pass
 
+
 def select_random_my_shrub():
     try:
         shrub_cards = wait.until(EC.presence_of_all_elements_located((
@@ -212,14 +240,13 @@ def select_random_my_shrub():
 
         selected_shrub = random.choice(shrub_cards)
         driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", selected_shrub)
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.cursor-pointer.flex.flex-wrap.items-center.justify-center.rounded-lg")))
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, "div.cursor-pointer.flex.flex-wrap.items-center.justify-center.rounded-lg")))
         selected_shrub.click()
     except Exception as e:
         print(f"[ERROR] Failed to select shrub: {e}")
         driver.save_screenshot("error_select_shrub.png")
         raise
-
-
 
 
 # ============================== TEST CASES ==============================
